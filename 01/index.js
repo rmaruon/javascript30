c692ba1bfde215daebfa50762e1d7145c2d03432
rmaruon <22119585+rmaruon@users.noreply.github.com>
2020-04-12 10:32:45 +0900

    Add: 押したキーを光らせる

--- 01/index.js
+++ 01/index.js
@@ -1,10 +1,20 @@
 document.addEventListener("keypress", playDrum, false);
+const keys = Array.from(document.querySelectorAll(".key"));
+for (key of keys) {
+  key.addEventListener("transitionend", removePlayingClass, false);
+}
 
 function playDrum(event) {
   const pressedKey = event.key;
   const keyElem = document.querySelector(`div[data-key="${pressedKey}"]`);
   const audioElem = document.querySelector(`audio[data-key=${pressedKey}]`);
 
+  keyElem.classList.add("playing");
+
   audioElem.currentTime = 0;
   audioElem.play();
+}
+
+function removePlayingClass(event) {
+  this.classList.remove("playing");
 }
